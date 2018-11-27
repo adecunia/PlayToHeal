20e1278acaa768fb571466010c3e071248d1b037
sndrmdrsbr <mederos.sandra18@gmail.com>
2018-11-27 09:48:42 +0100

    Add Cell class

---
+++ PlayToHeal/PlayToHeal/Source/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayToHeal.Source
+{
+    public class Cell
+    {
+        private int value;
+        private bool north;
+        private bool south;
+        private bool west;
+        private bool east;
+
+        public int Value { get { return value; } set { this.value = value; } }
+        public bool North { get { return north; } set { north = value; } }
+        public bool South { get { return south; } set { south = value; } }
+        public bool West { get { return west; } set { west = value; } }
+        public bool East { get { return east; } set { east = value; } }
+    }
+}
