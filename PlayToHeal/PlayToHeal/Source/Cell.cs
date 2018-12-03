60d0314ca4b7e6ad3764cf77a95582768488cac6
pBattas <ewenn.battas@devinci.fr>
2018-12-03 14:10:10 +0100

    oldest maze v1

--- PlayToHeal/PlayToHeal/Source/Cell.cs
+++ PlayToHeal/PlayToHeal/Source/Cell.cs
@@ -12,11 +12,30 @@ namespace PlayToHeal.Source
         private bool south;
         private bool west;
         private bool east;
+        private int x;
+        private int y;
 
         public int Value { get { return value; } set { this.value = value; } }
         public bool North { get { return north; } set { north = value; } }
         public bool South { get { return south; } set { south = value; } }
         public bool West { get { return west; } set { west = value; } }
         public bool East { get { return east; } set { east = value; } }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+
+
+
+
+        public Cell()
+        {
+            north = false;
+            south = false;
+            east = false;
+            west = false;
+        }
+
     }
+
 }
--- PlayToHeal/PlayToHeal/Source/Maze.cs
+++ PlayToHeal/PlayToHeal/Source/Maze.cs
@@ -15,9 +15,119 @@ namespace PlayToHeal.Source
 
 
         // BoardGame Generation (To do)
-        public char[][] GenerateBoardGame()
+        public Cell[,] GenerateBoardGame(int h, int w)
         {
+            Cell[,] boardGame = new Cell[h, w];
+            boardGame = properCellInitialisation(boardGame);
+
+            List<Cell> path = new List<Cell>();
+
+            //Select a random cell
+            path.Add(randomCell(boardGame));
+            Cell actual = path[0];
+            int actualIndex = 0;
+
+            while(!hasNoUnvisitedNb(path[path.Count - 1]))
+            {
+                path.Add(selectOneUnvisited(actual, boardGame));
+
+                if (hasNoUnvisitedNb(actual))
+                {
+                    actual = path[actualIndex + 1];
+                    actualIndex++;
+                }
+            }
+
+
+
             return boardGame;
+        }
+
+        // Choose random cell
+        public Cell randomCell(Cell[,] boardGame)
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(boardGame.GetLength(0));
+            int y = rnd.Next(boardGame.GetLength(1));
+
+            return boardGame[x, y];
+        }
+
+        public Cell selectOneUnvisited(Cell cell, Cell[,] boardGame)
+        {
+            int count = 0;
+
+            if(cell.West == false) { count++;  };
+            if(cell.East == false) { count++; };
+            if(cell.North == false) { count++; };
+            if(cell.South == false) { count++; };
+
+            Random rnd = new Random();
+            int i = rnd.Next(count);
+
+            switch (i)
+            {
+                case 0: //Case West
+                    cell.West = true;
+                    return boardGame[cell.X, cell.Y - 1];
+                case 1: //Case East
+                    cell.East = true;
+                    return boardGame[cell.X, cell.Y + 1];
+                case 2: //Case North
+                    cell.North = true;
+                    return boardGame[cell.X + 1, cell.Y];
+                case 3: //Case South
+                    cell.South = true;
+                    return boardGame[cell.X - 1, cell.Y];
+            }
+
+            //Si erreur
+            return cell;
+        }
+
+        public bool hasNoUnvisitedNb(Cell cell)
+        {
+            if(cell.West && cell.East && cell.North && cell.South)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Cell[,] properCellInitialisation(Cell[,] board)
+        {
+            for(int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    //If first row => North unavailable
+                    if (i == 0)
+                    {
+                        board[i, j].North = true;
+                    }
+                    //If last row => south unavailable
+                    if (i == board.GetLength(0))
+                    {
+                        board[i, j].South = false;
+                    }
+                    //If first column => West unavailable
+                    if (j == 0)
+                    {
+                        board[i, j].West = true;
+                    }
+                    //If last colum => East unavailable
+                    if(j == board.GetLength(1))
+                    {
+                        board[i, j].East = true;
+                    }
+
+                    //Initialize coordinates
+                    board[i, j].X = i;
+                    board[i, j].Y = j;
+                }
+            }
+
+            return board;
         }
     }
 }
